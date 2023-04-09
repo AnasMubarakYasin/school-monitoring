48c23dc8c3aef1deb8c3d5541dde899926c4c762
wahyuhidayat <91162157+WahyuSapurata@users.noreply.github.com>
2023-04-09 13:00:06 +0800

    fix answer student

--- resources/views/components/action/answer.blade.php
+++ resources/views/components/action/answer.blade.php
@@ -1,19 +1,23 @@
 @props([
     'item' => new \stdClass(),
 ])
-@if ($item->answer()->where('status', 'have been checked')->where('student_id', auth()->user()->id)->first())
-    <div id="edt_btn"
-        class="p-2 text-white bg-teal-400 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm text-center inline-flex items-center dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">
-        Done
-    </div>
+@if ($item->type == 'material')
+    -
 @else
-    <a id="edt_btn" href="{{ route('web.student.answer.view', ['materialAndAssignment' => $item]) }}"
-        class="p-2 text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm text-center inline-flex items-center dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor"
-            class="w-5 h-5">
-            <path stroke-linecap="round" stroke-linejoin="round"
-                d="M19.5 14.25v-2.625a3.375 3.375 0 00-3.375-3.375h-1.5A1.125 1.125 0 0113.5 7.125v-1.5a3.375 3.375 0 00-3.375-3.375H8.25m3.75 9v6m3-3H9m1.5-12H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 00-9-9z" />
-        </svg>
-        <span class="sr-only">Answer Button</span>
-    </a>
+    @if ($item->answer()->where('status', 'have been checked')->where('student_id', auth()->user()->id)->first())
+        <div id="edt_btn"
+            class="p-2 text-white bg-teal-400 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm text-center inline-flex items-center dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">
+            Done
+        </div>
+    @else
+        <a id="edt_btn" href="{{ route('web.student.answer.view', ['materialAndAssignment' => $item]) }}"
+            class="p-2 text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm text-center inline-flex items-center dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor"
+                class="w-5 h-5">
+                <path stroke-linecap="round" stroke-linejoin="round"
+                    d="M19.5 14.25v-2.625a3.375 3.375 0 00-3.375-3.375h-1.5A1.125 1.125 0 0113.5 7.125v-1.5a3.375 3.375 0 00-3.375-3.375H8.25m3.75 9v6m3-3H9m1.5-12H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 00-9-9z" />
+            </svg>
+            <span class="sr-only">Answer Button</span>
+        </a>
+    @endif
 @endif
